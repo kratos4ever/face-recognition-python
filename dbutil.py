533f71c6248de4bab33840398f86bc8b526314e5
Nishanth Vepachedu <nvep5898@orlawv027.na.convergys.com>
2018-03-02 16:55:28 -0500

    sample training images

--- dbutil.py
+++ dbutil.py
@@ -3,10 +3,14 @@ import os,sys
 
 db = MySQLdb.connect(host="orlawv027",user="biometric-user",passwd="B10M37R1K5",db="biometrics")
 cur = db.cursor()
-results = cur.execute(" select img, lan_id from train_img where status = 'N' ")
+nrows = cur.execute(" select image, lan_id from train_img where status = 'N' ")
+results = cur.fetchall()
 i = 0
 for rs in results:
-	img = open("./temp/train_"+rs[1]+"_%d",++i,"wb")
+	path = "./temp/train_"+rs[1]+"_%d.jpg"%i
+	i = i+1
+	print path
+	img = open(path,"wb")
 	img.write(rs[0])
 	img.close()
 

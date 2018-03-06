8a90a8a728d162a5ef6f4fb00bf777fd44de7f3f
Nishanth Vepachedu <nishant.vepachedu@gmail.com>
2018-03-06 08:02:38 -0700

    more notes on v1 for batchmode

--- batch-process.py
+++ batch-process.py
@@ -3,13 +3,20 @@ import sys,os
 import time
 import datetime
 
+def getConnection():
+	
+
 def main():
-	print("Hello world")
-	i = 0
-	while True:
-		print(">>",i)
-		time.sleep(60)
-		i = i+1
+	#get - connection to the mysql server - no externalizing as of now
+
+
+	#run the query to get the images from stream_img table, set into map<lanid,list<object:lanid,img,capture_time,id,status,etc>>
+
+	#Get a list of unique lanids from the above query and get the training image for each. set in a map<lanid,<object:lanid,id,image>>
+
+	#for each lanid in the stream_img result list, check if there is a training image, if there is, do the facial recog for each record 
+	#for the lanid, then write the status accordingly
+
 
 
 if __name__ == '__main__':

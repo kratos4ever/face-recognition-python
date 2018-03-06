02c5f17b1daa7358cc7ea8f6e8fb908e041cec1e
Nishanth Vepachedu <nishant.vepachedu@gmail.com>
2018-03-06 13:52:10 -0700

    moving towards data collection

--- batch-process.py
+++ batch-process.py
@@ -2,22 +2,85 @@
 import sys,os
 import time
 import datetime
+import MySQLdb
+import smtplib
+from face_data_classes import FaceStreamData
 
-def getConnection():
-	
+### INITIALIZES THE DB CONNECTION TO MYSQL
+def initDbConnection():
+	global db
+	db = MySQLdb.connect(host="localhost",user="biometric-user",passwd="B10M37R1K5",db="biometrics")
+	global cur
+	cur = db.cursor()
+
+
+### GETS THE UNPROCESSED RECORDS FROM STREAM_IMG TABLE
+def getUnProcessedStreamImages():
+
+	sql = " select lan_id,date_format(capture_time,'%m%d%Y_%H%i%S') as time, image,id  from stream_img where status = 'N' order by lan_id,id "
+	nrows = cur.execute(sql)
+	resultset = cur.fetchall()
+	global faceStrmLanIdSet 
+	global faceStrmMap
+	faceStrmMap = {}
+	faceStrmLanIdSet = set()
+
+	for data in resultset:
+		lanid = data[0]
+		capture_time = data[1]
+		image = data[2]
+		id = data[3]
+		status = "N"
+		streamData = FaceStreamData(lanid,id,capture_time,image,status)
+		
+		faceStrmLanIdSet.add(lanid)
+
+		if lanid in faceStrmMap:
+			faceStrmMap[lanid].append(streamData)
+		else:
+			imgDataArray = []
+			imgDataArray.append(streamData)
+			faceStrmMap[lanid]=imgDataArray
+
+	print(faceStrmLanIdSet)
+	print(faceStrmMap)
+
+
+def loadTrainingImages():
+	global faceTrainMap
+	faceTrainMap = {}
+
+	sql = " select a.lan_id,a.id,a.capture_time,a.image,a.processed_time,a.status,a.result from train_img a, " + 
+	"(select lan_id, max(id) as id from train_img where lan_id in ("++") group by lan_id) b where a.id = b.id "
+
 
 def main():
-	#get - connection to the mysql server - no externalizing as of now
+	print("Starting the batch-process iteration:")
+	
+	try:
+		#init - connection to the mysql server - no externalizing as of now
+		initDbConnection()
+
+	except MySQLdb.Error as e:
+		#### if cur is null or db is null then DB connection failed -> print error/email and exit
+		print("Error in getting connection:",e)
+		sys.exit(1)
 
 
-	#run the query to get the images from stream_img table, set into map<lanid,list<object:lanid,img,capture_time,id,status,etc>>
+	try:
+		#run the query to get the images from stream_img table, set into map<lanid,list<object:lanid,img,capture_time,id,status,etc>>	
+		getUnProcessedStreamImages()
 
-	#Get a list of unique lanids from the above query and get the training image for each. set in a map<lanid,<object:lanid,id,image>>
+		#Get a list of unique lanids from the above query and get the training image for each. set in a map<lanid,<object:lanid,id,image>>
+		loadTrainingImages()
+	
+	except Error as e:
+		print("Error while processing images:",e)
+	
 
 	#for each lanid in the stream_img result list, check if there is a training image, if there is, do the facial recog for each record 
 	#for the lanid, then write the status accordingly
 
 
-
 if __name__ == '__main__':
-		main()	
+	main()	

d48f8b7aacac78ed646e617a3107f12e5609d78f
Nishanth Vepachedu <nishanth.vepachedu@convergys.com>
2018-03-20 15:59:18 -0600

    added modes to the run for training/streaming selection on docker run

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo $MODE
 
-echo "Running the batch process"
+echo "Running the consumer process for $MODE"
 
-python3 -u consumer.py
+if ["$MODE" == "FaceStreaming"]; then
+	python3 -u consumer.py
+elif ["$MODE" == "FaceTraining"]; then
+	python3 -u training_consumer.py
+else
+	echo "Invalid mode, allowed values are FaceTraining OR FaceStreaming"
+fi

a90701ef5ce6a6633255707bb911a9286f27aed3
Nishanth Vepachedu <nishanth.vepachedu@convergys.com>
2018-03-20 16:20:14 -0600

    correction to run bash file

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 echo $MODE
 
 echo "Running the consumer process for $MODE"
-
-if ["$MODE" == "FaceStreaming"]; then
+FTRN="FaceStreaming"
+FSTR="FaceTraining"
+if [[ $MODE = $FSTR ]]
+then
 	python3 -u consumer.py
-elif ["$MODE" == "FaceTraining"]; then
+elif [[ $MODE = $FTRN ]]
+then
 	python3 -u training_consumer.py
 else
 	echo "Invalid mode, allowed values are FaceTraining OR FaceStreaming"

0c55a0480f880ea6d0d924657711718ddc5def3d
Nishanth Vepachedu <nishanth.vepachedu@convergys.com>
2018-03-28 19:20:16 -0600

    simple script for deploy

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+cd ~/bio-source
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter name=bio_* --format="{{.ID}}"))
+cd ./face-recognition-python
+git pull
+cd ..
+sudo docker build -t biometric-sense/bio1 --rm=true -f ./Dockerfile . 
+sudo docker run -d -e MODE=FaceStreaming --name bio_fstream biometric-sense/bio1:latest 
+sudo docker run -d -e MODE=FaceTraining --name bio_ftrain biometric-sense/bio1:latest 
+
